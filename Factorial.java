c202cae5c6fa15fb7916ae82ddd9009580c02266
Radhika-shinde <111429923+Radhika-shinde@users.noreply.github.com>
2023-03-31 00:35:58 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		int factorial=1;
+	
+	    for(int i=1;i<=5;i++) {
+		factorial=factorial*i;
+	
+	}
+	    System.out.print("factorial is " +factorial);
+
+}
+}
